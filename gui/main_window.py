1737ec2c221dc88201b67ccbc1ab6460d29767ff
Spandan7724 <spandanchavan727477@gmail.com>
2024-07-27 23:01:08 +0530

    tools-eraser,paintbrush

--- gui/main_window.py
+++ gui/main_window.py
@@ -1,7 +1,9 @@
 import tkinter as tk
-from tkinter import filedialog
-from PIL import Image, ImageTk
+from tkinter import filedialog, Menu
+from PIL import Image, ImageTk, ImageDraw
 from image_processing.editor import ImageEditor
+from .toolbar import ToolBar
+
 
 class MainWindow:
     def __init__(self, root):
@@ -11,9 +13,12 @@ class MainWindow:
 
         self.image = None
         self.image_path = None
+        self.draw = None
 
         self.canvas = tk.Canvas(root, bg='white')
         self.canvas.pack(fill=tk.BOTH, expand=True)
+        self.canvas.bind('<B1-Motion>', self.paint)
+        self.canvas.bind('<ButtonPress-1>', self.start_paint)
 
         menubar = tk.Menu(root)
         root.config(menu=menubar)
@@ -29,22 +34,31 @@ class MainWindow:
         edit_menu.add_command(label="Rotate", command=self.rotate_image)
         menubar.add_cascade(label="Edit", menu=edit_menu)
 
+        self.toolbar = ToolBar(root, self)
+        menubar.add_cascade(label="Tools", menu=self.toolbar.menu)
+
+        self.current_tool = 'brush'
+        self.prev_x = None
+        self.prev_y = None
+
     def open_image(self):
-        file_path = filedialog.askopenfilename(filetypes=[("Image files", "*.jpg *.jpeg *.png *.bmp")])
+        file_path = filedialog.askopenfilename(
+            filetypes=[("Image files", "*.jpg *.jpeg *.png *.bmp")])
         if file_path:
             self.image_path = file_path
             self.image = Image.open(file_path)
             self.display_image()
+            self.draw = ImageDraw.Draw(self.image)
 
     def save_image(self):
         if self.image:
-            file_path = filedialog.asksaveasfilename(defaultextension=".png", filetypes=[("PNG files", "*.png"), ("JPEG files", "*.jpg *.jpeg"), ("Bitmap files", "*.bmp")])
+            file_path = filedialog.asksaveasfilename(defaultextension=".png", filetypes=[(
+                "PNG files", "*.png"), ("JPEG files", "*.jpg *.jpeg"), ("Bitmap files", "*.bmp")])
             if file_path:
                 self.image.save(file_path)
 
     def display_image(self):
-        image = self.image.copy()
-        self.tk_image = ImageTk.PhotoImage(image)
+        self.tk_image = ImageTk.PhotoImage(self.image)
         self.canvas.create_image(0, 0, anchor=tk.NW, image=self.tk_image)
         self.canvas.config(scrollregion=self.canvas.bbox(tk.ALL))
 
@@ -55,6 +69,7 @@ class MainWindow:
             editor.save('cropped.png')
             self.image = Image.open('cropped.png')
             self.display_image()
+            self.draw = ImageDraw.Draw(self.image)
 
     def resize_image(self):
         if self.image:
@@ -63,6 +78,7 @@ class MainWindow:
             editor.save('resized.png')
             self.image = Image.open('resized.png')
             self.display_image()
+            self.draw = ImageDraw.Draw(self.image)
 
     def rotate_image(self):
         if self.image:
@@ -70,4 +86,20 @@ class MainWindow:
             editor.rotate(90)  # Example rotation
             editor.save('rotated.png')
             self.image = Image.open('rotated.png')
+            self.display_image()
+            self.draw = ImageDraw.Draw(self.image)
+
+    def start_paint(self, event):
+        self.prev_x, self.prev_y = event.x, event.y
+
+    def paint(self, event):
+        if self.image and self.draw:
+            x, y = event.x, event.y
+            if self.current_tool == 'brush':
+                self.draw.line([self.prev_x, self.prev_y, x, y],
+                               fill=self.toolbar.brush_color, width=self.toolbar.brush_size)
+            elif self.current_tool == 'eraser':
+                self.draw.line([self.prev_x, self.prev_y, x, y],
+                               fill='white', width=self.toolbar.eraser_size)
+            self.prev_x, self.prev_y = x, y
             self.display_image()
